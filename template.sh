63257e45bef2869568a5f22bc0a6d22585fd1328
SatyamSakral <satyamsakral@gmail.com>
2025-08-02 11:27:03 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,14 @@
+#directory
+mkdir -p src
+mkdir -p research
+#files
+touch src/__init__.py
+touch src/helpers.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "files created successfully."
